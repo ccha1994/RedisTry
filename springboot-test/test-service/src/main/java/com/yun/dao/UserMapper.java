b798c2e1576cf43a016573fc76066e6bdee54741
wangyunlong <wangyunlong@corp.netease.com>
2018-07-03 15:29:16 +0800

    @dev 使用spring cache

--- springboot-test/test-service/src/main/java/com/yun/dao/UserMapper.java
+++ springboot-test/test-service/src/main/java/com/yun/dao/UserMapper.java
@@ -12,24 +12,22 @@ import java.util.List;
 @CacheConfig(cacheNames = "users")
 public interface UserMapper {
     @Insert("insert into user( name , age ) values( #{name} , #{age})")
-    @CachePut
-    //@Options(useGeneratedKeys = true, keyProperty = "id")
+    @CacheEvict(allEntries = true)
     int addUser(@Param("name") String name,@Param("age") String age);
 
-    @Select("select * from user where id = #{uid}")
-    @Cacheable
-    User findById(@Param("uid") Integer id);
+    @Select("select * from user where id = #{id}")
+    @Cacheable(key = "#p0")
+    User findById(@Param("id") Integer id);
 
     @Select("select * from user where 1=1")
     @Cacheable
     List<User> findAll();
 
     @Update("update user set name=#{name} where id=#{id}")
-    @CachePut
+    @CacheEvict(key = "#p0")
     void updataById(@Param("id")Integer uid,@Param("name")String uname);
 
-    //如果指定为 true，则方法调用后将立即清空所有缓存
     @Delete("delete from user where id=#{id}")
-    @CacheEvict(allEntries = true)
+    @CacheEvict(key = "#p0")
     void deleteById(@Param("id")Integer id);
 }
--- springboot-test/test-service/src/main/java/com/yun/util/RedisConfig.java
+++ springboot-test/test-service/src/main/java/com/yun/util/RedisConfig.java
@@ -32,35 +32,6 @@ public class RedisConfig extends CachingConfigurerSupport {
     @Value("${spring.redis.timeout}")
     private int timeout;
 
-    //自定义缓存key生成策略
-    /*@Bean
-    public KeyGenerator keyGenerator() {
-        return new KeyGenerator(){
-            @Override
-            public Object generate(Object target, java.lang.reflect.Method method, Object... params) {
-                StringBuffer sb = new StringBuffer();
-                sb.append(target.getClass().getName());
-                sb.append(method.getName());
-                for(Object obj:params){
-                    sb.append(obj.toString());
-                }
-                return sb.toString();
-            }
-        };
-    }*/
-
-    /**
-     * 缓存管理器
-     *
-     */
-    /*@Bean
-    public CacheManager cacheManager(@SuppressWarnings("rawtypes") RedisTemplate redisTemplate) {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
-        //设置缓存过期时间
-        cacheManager.setDefaultExpiration(10000);
-        return cacheManager;
-    }*/
-
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory){
         StringRedisTemplate template = new StringRedisTemplate(factory);
