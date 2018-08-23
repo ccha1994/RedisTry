18bf776fd4c777c3cfa2668805e6219bcacac638
wangyunlong <wangyunlong@corp.netease.com>
2018-08-23 14:22:49 +0800

    @dev merge

--- springboot-test/test-service/src/main/java/com/yun/util/RedisConfig.java
+++ springboot-test/test-service/src/main/java/com/yun/util/RedisConfig.java
@@ -4,13 +4,10 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -18,8 +15,8 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 
 /**
  * Redis缓存配置类
- * @author wangyunlong
  *
+ * @author wangyunlong
  */
 @Configuration
 @EnableCaching
@@ -33,7 +30,7 @@ public class RedisConfig extends CachingConfigurerSupport {
     private int timeout;
 
     @Bean
-    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory){
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         StringRedisTemplate template = new StringRedisTemplate(factory);
         //设置序列化工具
         setSerializer(template);
@@ -41,8 +38,8 @@ public class RedisConfig extends CachingConfigurerSupport {
         return template;
     }
 
-    private void setSerializer(StringRedisTemplate template){
-        @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void setSerializer(StringRedisTemplate template) {
+        @SuppressWarnings({"rawtypes", "unchecked"})
         Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
@@ -50,4 +47,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         jackson2JsonRedisSerializer.setObjectMapper(om);
         template.setValueSerializer(jackson2JsonRedisSerializer);
     }
+
+    //////////////
 }
